4e49395bf934dbf61fac7fc5633f02d75563eb20
Shiretomi <shiretomi@gmail.com>
2025-11-22 20:12:33 +0300

    service to check new tickets

---
+++ src/services/ticket_tracker/ticket_tracker.py
@@ -0,0 +1,40 @@
+import asyncio
+import time
+
+from aiogram import Bot, html
+from aiogram.enums import ParseMode
+from loguru import logger
+from common.utils.tickets_api import TicketsAPI
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = getenv("TELEGRAM_TOKEN")
+CHAT_ID = getenv("CHAT_ID")
+
+bot = Bot(TOKEN)
+
+
+def mention_new_ticket(tickets):
+    for ticket in tickets:
+        msg = f'''🟢 Новый тикет 🟢                         
+                \n\
+                \n{html.link(html.bold(ticket.ticket_id), f"https://tracker.ntechlab.com/tickets/{ticket.ticket_id}")}\
+                \n\
+                \n{ticket.name}\
+            '''
+        bot.send_message(chat_id=CHAT_ID,)
+        
+
+
+def polling():
+    api = TicketsAPI()
+    while True:
+        tickets = api.get_new_ticket()
+        if len(tickets.ticket_id) != 0:
+            mention_new_ticket(tickets)
+        time.sleep(300)
+
+if __name__ == "__main__":
+    asyncio.run(polling())
