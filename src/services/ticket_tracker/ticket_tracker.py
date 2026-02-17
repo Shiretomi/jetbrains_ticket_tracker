d91d9c7a8f4dfd120457cee9c1fc70fb195c95bf
Shiretomi <shiretomi@gmail.com>
2026-02-17 20:51:17 +0300

    bs4 formatting add

--- src/services/ticket_tracker/ticket_tracker.py
+++ src/services/ticket_tracker/ticket_tracker.py
@@ -7,6 +7,7 @@ import redis
 import html as html_py
 import re
 
+from bs4 import BeautifulSoup, Comment
 from utils import kb
 from aiogram import Bot, html
 from aiogram.enums import ParseMode
@@ -34,6 +35,19 @@ r = redis.Redis(
 
 bot = Bot(TOKEN)
 
+
+def clean_ms_word_html(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    for comment in soup.find_all(string=lambda text: isinstance(text, Comment)):
+        comment.extract()
+        
+    for tag in soup.find_all(True):
+        tag.attrs = {name: value for name, value in tag.attrs.items() 
+                     if not name.startswith('mso-')}
+        
+    return soup.prettify()
+
 def format_text(text):
     return re.sub(r'<[^>]+>', '', text)
 
@@ -41,9 +55,8 @@ async def mention_new_ticket(tickets):
     for ticket in tickets:
         if r.sismember("tickets:new", ticket.ticket_id):
             continue
-
-        description_raw = html.expandable_blockquote(html_py.escape(format_text(ticket.description)).strip())
-        description = description_raw[:3500] + "..."
+        description_raw = clean_ms_word_html(ticket.description)[:3500] + "..."
+        description = html.expandable_blockquote(html_py.escape(format_text(description_raw)).strip())
 
         msg = f'''🟢 Новый тикет 🟢\n\n{html.bold("ID:")} {html.link(html.bold(ticket.ticket_id), f"https://tracker.ntechlab.com/tickets/{ticket.ticket_id}")}\
                 \n\n{html.bold("Название")}: {html_py.escape(format_text(ticket.name))}\
@@ -54,7 +67,7 @@ async def mention_new_ticket(tickets):
         await bot.send_message(chat_id=CHAT_ID, text=msg, parse_mode=ParseMode.HTML, reply_markup=await kb.spam_button(ticket.ticket_id), reply_to_message_id=CHAT_THREAD)
         logger.info(f"Ticket {ticket.ticket_id} mentioned.")
         r.sadd("tickets:new", ticket.ticket_id)
-                
+
 
 async def polling():
     api = TicketsAPI()
