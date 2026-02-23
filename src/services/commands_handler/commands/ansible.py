27099db952b402c82ef43c4ecc8084f49a2888d0
Shiretomi <shiretomi@gmail.com>
2026-02-24 02:51:50 +0300

    ansible text

--- src/services/commands_handler/commands/ansible.py
+++ src/services/commands_handler/commands/ansible.py
@@ -18,13 +18,13 @@ def init_ansible(bot):
     @bot.message(Command("update_supdemo"))
     async def update(message: Message):
         sent_message = await message.answer(f"Обновление SupDemo:\nПрогресс:\n\n")
-        await run_ansible_playbook(sent_message.chat.id, sent_message.message_id)
+        await run_ansible_playbook(sent_message.chat.id, sent_message.message_id, sent_message.text)
 
     async def track_download_progress(chat_id, message_id, total_size, file_path):
         total_gb = total_size / (1024 ** 3)
         pass
 
-    async def run_ansible_playbook(chat_id, message_id):
+    async def run_ansible_playbook(chat_id, message_id, original_text):
         path_to_playbook = os.path.join("/app", "ansible", "update.yml")
         path_to_hosts = os.path.join("/app", "ansible", "hosts.ini")
         
@@ -35,6 +35,6 @@ def init_ansible(bot):
         )
         stdout, stderr = await result.communicate()
         if result.returncode == 0:
-            await BOT.edit_message_text(chat_id, message_id, "✅ Обновление успешно завершено!")
+            await BOT.edit_message_text(chat_id, message_id, text=original_text + "✅ Обновление успешно завершено!")
         else:
-            await BOT.edit_message_text(chat_id, message_id, f"❌ Ошибка:\n{stderr.decode()}")
+            await BOT.edit_message_text(chat_id, message_id, text=original_text + f"\n❌ Ошибка при обновлении:\n{stderr.decode()}")
